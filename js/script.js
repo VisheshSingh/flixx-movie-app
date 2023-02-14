06c131ec13466c21307425968080e8a851a08fab
Vishesh Singh <VisheshSingh16@gmail.com>
2023-02-14 14:30:37 -0500

    Page router and active links

--- js/script.js
+++ js/script.js
@@ -2,7 +2,33 @@ const global = {
   currentPage: window.location.pathname,
 };
 
-function init() {}
+function displayActivePage() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/movies.html':
+      console.log('Movies');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/tv-details.html':
+      console.log('TV show details');
+      break;
+  }
+  displayActivePage();
+}
 
 // Event listeners
 document.addEventListener('DOMContentLoaded', init);
